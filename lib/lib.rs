eaf035fce03d3421ede5e0de4358ca02c4a1c3a9
Marcelo Vieira Marchesi <marcelovieira.marchesi@gmail.com>
2025-04-24 18:06:44 -0300

    Is Bazel calling Cargo? It works this way too.

--- lib/lib.rs
+++ lib/lib.rs
@@ -2,7 +2,7 @@
 // In this case C++ code is calling the function below.
 // To be able to link it with C++ object code, the crate_type needed to be
 // set here as this option still isn't supported in Bazel rust_rules.
-#![crate_type = "cdylib"]
+//#![crate_type = "cdylib"]
 #[unsafe(no_mangle)]
 pub extern "C" fn print_message(message: *const std::os::raw::c_char) {
     let c_str = unsafe {
